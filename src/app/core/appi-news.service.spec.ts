affb6381ce827fe2744de3e2bf1ca757e9428b6f
Edjajim <eduardo_jara_26@hotmail.com>
2024-09-17 22:00:21 -0600

    Creación de la estructura

---
+++ src/app/core/appi-news.service.spec.ts
@@ -0,0 +1,16 @@
+import { TestBed } from '@angular/core/testing';
+
+import { AppiNewsService } from './appi-news.service';
+
+describe('AppiNewsService', () => {
+  let service: AppiNewsService;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({});
+    service = TestBed.inject(AppiNewsService);
+  });
+
+  it('should be created', () => {
+    expect(service).toBeTruthy();
+  });
+});
