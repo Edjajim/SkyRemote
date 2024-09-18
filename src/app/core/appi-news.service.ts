8a6866b6890fb577c82385e89c0e108f39039a9e
Edjajim <eduardo_jara_26@hotmail.com>
2024-09-18 00:26:38 -0600

    Conectada la Api de noticias

--- src/app/core/appi-news.service.ts
+++ src/app/core/appi-news.service.ts
@@ -1,9 +1,19 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AppiNewsService {
 
-  constructor() { }
+  constructor(private httpClient: HttpClient) { }
+
+  private API_KEY = "2251200a593f4c2ab54955cd59921abb"
+  private API_URL = "https://newsapi.org/v2/everything"
+
+  getNews(region: string): Observable<any> {
+    const query = 'weather OR climate OR clima OR huracán OR tormenta';
+    return this.httpClient.get<any>(`${this.API_URL}?q=${query}&language=${region}&apiKey=${this.API_KEY}`);
+  }
 }
--- src/app/ui/news/news.component.ts
+++ src/app/ui/news/news.component.ts
@@ -1,12 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { AppiNewsService } from '../../core/appi-news.service';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-news',
   standalone: true,
-  imports: [],
-  templateUrl: './news.component.html',
-  styleUrl: './news.component.css'
+  imports: [CommonModule],
+  templateUrl: './news.component.html'
 })
-export class NewsComponent {
+export class NewsComponent implements OnInit{
+
+  constructor(private appi: AppiNewsService) {}
+  
+  newsArticles: any[] = [];
+  
+  ngOnInit(): void {
+    this.appi.getNews("es").subscribe(
+      (response) => {
+        this.newsArticles = response.articles;
+      },
+      (error) => {
+        console.error('Error fetching news:', error);
+      }
+    );
+  }
 
 }
